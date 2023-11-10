bee1c1579ad4901e8ee654d4e9b5ee872ed04445
marchewazz <mateuszooospot@gmail.com>
2023-11-10 18:49:30 +0100

    Driver model properties change

--- models/Driver.model.ts
+++ models/Driver.model.ts
@@ -4,21 +4,21 @@ export default interface Driver {
     points: string,
     wins: string,
     Driver: {
-      "driverId": string,
-      "permanentNumber": string,
-      "code": string,
-      "url": string,
-      "givenName": string,
-      "familyName": string,
-      "dateOfBirth": string,
-      "nationality": string
+      driverId: string,
+      permanentNumber: string,
+      code: string,
+      url: string,
+      givenName: string,
+      familyName: string,
+      dateOfBirth: string,
+      nationality: string
     },
     Constructors: [
       {
-        "constructorId": string,
-        "url": string,
-        "name": string,
-        "nationality": string
+        constructorId: string,
+        url: string,
+        name: string,
+        nationality: string
       }
     ]
   }
