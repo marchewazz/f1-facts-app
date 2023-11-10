231e68a366a6cccb5e8d004df8cf843b5be5fb93
marchewazz <mateuszooospot@gmail.com>
2023-11-10 18:56:07 +0100

    Constructors standings screen

---
+++ models/Constructor.model.ts
@@ -0,0 +1,12 @@
+export default interface Constructor {
+    position: string,
+    positionText: string,
+    points: string,
+    wins: string,
+    Constructor: {
+      constructorId: string,
+      url: string,
+      name: string,
+      nationality: string
+    }
+  }
--- screens/ConstructorsStandingsScreen.tsx
+++ screens/ConstructorsStandingsScreen.tsx
@@ -1,9 +1,90 @@
 import { View, Text } from "react-native";
+import Constructor from "../models/Constructor.model";
+import { useEffect, useState } from "react";
+import { Dropdown } from "react-native-element-dropdown";
 
 export default function ConstructorsStandingsScreen() {
+    const [years, setYears] = useState<{
+        year: string
+    }[]>([
+        {
+            year: ""
+        }
+    ]);
+
+    const [value, setValue] = useState<{
+        year: string
+    }>({year: "2023"});
+
+    const [standings, setStandings] = useState<Constructor[]>([]);
+
+    const [ready, setReady] = useState<boolean>(false)
+
+    async function getStandings() {
+        try {
+            setReady(false)
+            const response = await fetch(`http://ergast.com/api/f1/${value.year}/constructorStandings.json`)
+            const json = await response.json();
+            // console.log(json.MRData.StandingsTable.StandingsLists[0].ConstructorStandings);
+            setStandings(json.MRData.StandingsTable.StandingsLists[0].ConstructorStandings as Constructor[]);
+          } catch (error) {
+            console.error(error);
+          } finally {
+            setReady(true);
+          }
+    }
+
+    useEffect(() => {
+        getStandings()
+    }, [value])
+    
+
+    useEffect(() => {
+        let years: {
+            year: string
+        }[]  = []
+
+        for (let i = 2023; i >= 1958; i--) {
+            years.push({
+                year: i.toString()
+            })
+        }
+
+        setYears(years)
+    }, [])
+
     return (
         <View>
-            <Text>Constructors standings</Text>
+            <Dropdown 
+                data={years} 
+                labelField="year"
+                valueField="year"
+                value={value}
+                placeholder="Choose a year..."
+                onChange={(value) => {
+                    setValue(value)
+                }}
+            />
+            { ready ? (
+                <>
+                    { standings.map((constructor: Constructor) => {
+                        return (
+                            <View className="flex flex-row justify-between">
+                                <Text>
+                                    { constructor.Constructor.name }
+                                </Text>
+                                <Text>
+                                    { constructor.points }
+                                </Text>
+                            </View>
+                        )
+                    })}
+                </>
+            ) : (
+                <Text>
+                    Loading...
+                </Text>
+            )}
         </View>
     )
 }
