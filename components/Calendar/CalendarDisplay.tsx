eb70a5d0b95bd84781fa35836a9572dfa9b68bdb
marchewazz <mateuszooospot@gmail.com>
2023-11-11 16:51:13 +0100

    Calendar display

---
+++ components/Calendar/CalendarDisplay.tsx
@@ -0,0 +1,90 @@
+import { useEffect, useState } from "react";
+import { View, Text } from "react-native";
+import { Dropdown } from "react-native-element-dropdown";
+import RaceSchedule from "../../models/RaceSchedule.model";
+
+export default function CalendarDisplay() {
+    const [years, setYears] = useState<{
+        year: string
+    }[]>([
+        {
+            year: ""
+        }
+    ]);
+
+    const [value, setValue] = useState<{
+        year: string
+    }>({year: "2023"});
+
+    const [calendar, setCalendar] = useState<RaceSchedule[]>([]);
+
+    const [ready, setReady] = useState<boolean>(false)
+
+    async function getCalendar() {
+        try {
+            setReady(false)
+            const response = await fetch(`http://ergast.com/api/f1/${value.year}.json`)
+            const json = await response.json();
+           
+            setCalendar(json.MRData.RaceTable.Races as RaceSchedule[]);
+          } catch (error) {
+            console.error(error);
+          } finally {
+            setReady(true);
+          }
+    }
+
+    useEffect(() => {
+        getCalendar()
+    }, [value])
+    
+
+    useEffect(() => {
+        let years: {
+            year: string
+        }[]  = []
+
+        for (let i = 2023; i >= 1950; i--) {
+            years.push({
+                year: i.toString()
+            })
+        }
+
+        setYears(years)
+    }, [])
+
+    return (
+        <View>
+            <Dropdown 
+                data={years} 
+                labelField="year"
+                valueField="year"
+                value={value}
+                placeholder="Choose a year..."
+                onChange={(value) => {
+                    setValue(value)
+                }}
+            />
+            { ready ? (
+                <>
+                    { calendar.map((race: RaceSchedule) => {
+                        return (
+                            <View className="flex flex-row justify-between">
+                                <Text>
+                                    { race.raceName }
+                                </Text>
+                                <Text>
+                                    { race.date }
+                                </Text>
+                            </View>
+                        )
+                    })}
+                </>
+            ) : (
+                <Text>
+                    Loading...
+                </Text>
+            )}
+        </View>
+    )
+}
--- screens/CalendarScreen.tsx
+++ screens/CalendarScreen.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from "react-native";
 import NextRaceCountdown from "../components/Calendar/NextRaceCountdown";
+import CalendarDisplay from "../components/Calendar/CalendarDisplay";
 
 export default function CalendarScreen() {
     return (
         <View>
             <NextRaceCountdown />
+            <CalendarDisplay />
         </View>
     )
 }
