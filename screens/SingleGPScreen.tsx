580d10fc1c304740661ebab4b3a9a10f1272fd98
marchewazz <mateuszooospot@gmail.com>
2023-11-15 15:05:16 +0100

    Labels related to weekends format

--- screens/SingleGPScreen.tsx
+++ screens/SingleGPScreen.tsx
@@ -9,31 +9,80 @@ export default function SingleGPScreen(props: any) {
     return (
         <View>
             <Text>{`${schedule.season} ${schedule.raceName}` }</Text>
-            { schedule.FirstPractice ? (
-                <Text>
-                    {`First practice: ${schedule.FirstPractice.date} ${schedule.FirstPractice.time}`}
-                </Text>
-            ) : (null)}
-            { schedule.SecondPractice ? (
-                <Text>
-                    {`Second practice: ${schedule.SecondPractice.date} ${schedule.SecondPractice.time}`}
-                </Text>
-            ) : (null)}
-            { schedule.ThirdPractice ? (
-                <Text>
-                    {`Thrid practice: ${schedule.ThirdPractice.date} ${schedule.ThirdPractice.time}`}
-                </Text>
-            ) : (null)}
-            { schedule.Qualifying ? (
-                <Text>
-                    {`Qualifying: ${schedule.Qualifying.date} ${schedule.Qualifying.time}`}
-                </Text>
-            ) : (null)}
-            { schedule.Sprint ? (
-                <Text>
-                    {`Sprint: ${schedule.Sprint.date} ${schedule.Sprint.time}`}
-                </Text>
-            ) : (null)}
+            { schedule.Sprint  ? (
+                <>
+                    { schedule.season === "2023" ? (
+                        <>
+                            { schedule.FirstPractice ? (
+                                <Text>
+                                    {`First practice: ${schedule.FirstPractice.date} ${schedule.FirstPractice.time ?? ""}`}
+                                </Text>
+                            ) : (null)}
+                            { schedule.Qualifying ? (
+                                <Text>
+                                    {`Qualifying: ${schedule.Qualifying.date} ${schedule.Qualifying.time ?? ""}`}
+                                </Text>
+                            ) : (null)}
+                            { schedule.SecondPractice ? (
+                                <Text>
+                                    {`Sprint qualifying: ${schedule.SecondPractice.date} ${schedule.SecondPractice.time ?? ""}`}
+                                </Text>
+                            ) : (null)}
+                            { schedule.Sprint ? (
+                                <Text>
+                                    {`Sprint: ${schedule.Sprint.date} ${schedule.Sprint.time ?? ""}`}
+                                </Text>
+                            ) : (null)}
+                        </>
+                    ) : (
+                        <>
+                            { schedule.FirstPractice ? (
+                                <Text>
+                                    {`First practice: ${schedule.FirstPractice.date} ${schedule.FirstPractice.time ?? ""}`}
+                                </Text>
+                            ) : (null)}
+                            { schedule.Qualifying ? (
+                                <Text>
+                                    {`Qualifying: ${schedule.Qualifying.date} ${schedule.Qualifying.time ?? ""}`}
+                                </Text>
+                            ) : (null)}
+                            { schedule.SecondPractice ? (
+                                <Text>
+                                    {`Second pracitce: ${schedule.SecondPractice.date} ${schedule.SecondPractice.time ?? ""}`}
+                                </Text>
+                            ) : (null)}
+                            { schedule.Sprint ? (
+                                <Text>
+                                    {`Sprint: ${schedule.Sprint.date} ${schedule.Sprint.time ?? ""}`}
+                                </Text>
+                            ) : (null)}
+                        </>
+                    )}
+                </>
+            ) : (
+                <>
+                    { schedule.FirstPractice ? (
+                        <Text>
+                            {`First practice: ${schedule.FirstPractice.date} ${schedule.FirstPractice.time ?? ""}`}
+                        </Text>
+                    ) : (null)}
+                    { schedule.SecondPractice ? (
+                        <Text>
+                            {`Second pracitce: ${schedule.SecondPractice.date} ${schedule.SecondPractice.time ?? ""}`}
+                        </Text>
+                    ) : (null)}
+                    { schedule.ThirdPractice ? (
+                        <Text>
+                            {`Thrid practice: ${schedule.ThirdPractice.date} ${schedule.ThirdPractice.time ?? ""}`}
+                        </Text>
+                    ) : (null)}
+                    { schedule.Qualifying ? (
+                        <Text>
+                            {`Qualifying: ${schedule.Qualifying.date} ${schedule.Qualifying.time ?? ""}`}
+                        </Text>
+                    ) : (null)}
+                </>
+            )}
             <Text>
                 {`Race: ${schedule.date} ${schedule.time ?? ""}`}
             </Text>
