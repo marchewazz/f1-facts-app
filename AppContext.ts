f2f9b98265c7a4c2c82fa76dfa1498901d9e8503
marchewazz <mateuszooospot@gmail.com>
2024-01-02 23:57:24 +0100

    Type definitions

--- AppContext.ts
+++ AppContext.ts
@@ -2,9 +2,9 @@ import { createContext } from "react";
 
 const AppContext = createContext<{
     audioOn: boolean | undefined,
-    setAudioOn: React.Dispatch<React.SetStateAction<boolean>> | undefined, 
+    setAudioOn: React.Dispatch<React.SetStateAction<boolean | undefined>> | undefined, 
     vibrationOn: boolean | undefined, 
-    setVibrationOn: React.Dispatch<React.SetStateAction<boolean>> | undefined
+    setVibrationOn: React.Dispatch<React.SetStateAction<boolean | undefined>> | undefined
 }>({
     audioOn: undefined,
     setAudioOn: undefined,
